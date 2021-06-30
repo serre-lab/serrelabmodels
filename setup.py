3ec212ec934e2ae0074dde08209c5878e03230cc
grassknoted <grassknoted@gmail.com>
2021-06-30 22:36:21 +0530

    adding setup configurations

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="inhouse-models",
+    version="0.0.1",
+    author='Akash Nagaraj',
+    author_email='akashn1897@gmail.com',
+    description='Package to import in-house models from the Serre Lab at Brown University.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/serre-lab/inhouse-models',
+    project_urls={
+        "Documentation": "https://github.com/serre-lab/inhouse-models/docs",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "models"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.5",
+)
