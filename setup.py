162ab0f7ed17b8aeba2a0b58dc03e1b8822eb788
Akash Nagaraj <anagara8@g18.ddns.brown.edu>
2021-09-20 17:05:01 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="inhouse_models",
-    version="0.0.1",
+    version="0.0.2",
     author='Akash Nagaraj',
     author_email='akash_n@brown.edu',
     description='Package to import in-house models from the Serre Lab at Brown University.',
