abfa74318047374d110b226e5d69b62ed114a728
Akash Nagaraj <anagara8@g18.ddns.brown.edu>
2021-09-20 17:23:32 -0400

    changing name

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="inhouse_models",
+    name="serrelabmodels",
     version="0.0.3",
     author='Akash Nagaraj',
     author_email='akash_n@brown.edu',
     description='Package to import in-house models from the Serre Lab at Brown University.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/serre-lab/inhouse_models',
+    url='https://github.com/serre-lab/serrelabmodels',
     project_urls={
-        "Documentation": "https://github.com/serre-lab/inhouse_models/docs",
+        "Documentation": "https://github.com/serre-lab/serrelabmodels/docs",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
