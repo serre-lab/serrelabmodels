d06c262788ac73d676be136f30852ed5990c00ff
Akash Nagaraj <anagara8@g18.ddns.brown.edu>
2021-09-20 16:57:36 -0400

    code cleanup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="serrelab-models",
+    name="inhouse_models",
     version="0.0.1",
     author='Akash Nagaraj',
     author_email='akash_n@brown.edu',
@@ -13,14 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/serre-lab/inhouse-models',
     project_urls={
-        "Documentation": "https://github.com/serre-lab/inhouse-models/docs",
+        "Documentation": "https://github.com/serre-lab/inhouse_models/docs",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "models"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
