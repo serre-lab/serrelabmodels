29b5d6f1cf42a9f9ebedab4ebf5d5a2d884745bd
anagara8 <anagara8@g18.ddns.brown.edu>
2021-09-20 16:29:49 -0400

    adding mock files

---
+++ src/hgru.py
@@ -0,0 +1,16 @@
+universal_text = "SerreLab"
+
+def hello():
+    print("Hello!")
+
+def model5():
+    model = 5
+    return model
+
+def model3():
+    model = 3
+    return model
+
+def modelAdd(a, b):
+    print("Running modelAdd")
+    return a + b
