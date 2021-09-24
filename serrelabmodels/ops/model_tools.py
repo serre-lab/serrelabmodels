9b85b1b66cd83b5e8ca8df71b6601a01023f7e43
Akash Nagaraj <akash_n@brown.edu>
2021-09-24 18:19:15 -0400

    added operations folder

---
+++ serrelabmodels/ops/model_tools.py
@@ -0,0 +1,8 @@
+import os
+from utils import py_utils
+
+def get_model(cfg):
+    print('importing ',cfg['import_prepath'], '.',cfg['import_class'])
+    model_module = py_utils.import_module(cfg['import_prepath'])
+    model = getattr(model_module, cfg['import_class'])(**cfg['args'])
+    return model
