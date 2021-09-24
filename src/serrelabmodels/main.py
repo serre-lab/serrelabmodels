c646c6a462bd2f7ec4a07d1038dfc5aabcba81a8
Akash Nagaraj <akash_n@brown.edu>
2021-09-24 18:19:49 -0400

    adding a main to setup configs

---
+++ src/serrelabmodels/main.py
@@ -0,0 +1,17 @@
+import os
+from serrelabmodels.ops import experiment_tools
+from serrelabmodels.utils import py_utils
+import sys
+import logging
+
+
+def main(cfg):
+    logging.info(cfg.pretty())
+
+if __name__ == "__main__":
+    cfg = py_utils.get_config(sys.argv[1:])
+    py_utils.setup_logging(cfg.dir)
+    print('PID:',os.getpid())
+    if 'gpus' in cfg and cfg.gpus is not None:
+        py_utils.allocate_gpus(cfg.gpus)
+    main(cfg)
